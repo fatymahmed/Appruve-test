d9a21d28ddbaabbc61742a2a8ce1e45a7be853b9
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-04 12:01:27 +0300

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className='Header'>
+      <div>
+        <div>
+          <p>Appruve</p>
+        </div>
+        <ul>
+          <li>Products</li>
+          <li>Company</li>
+          <li>Sales</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
